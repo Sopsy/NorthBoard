d6e89043cb17e44f888a86693427791edd0485bb
Sopsys <redir1@npn.fi>
2013-01-30 17:18:13 +0200

    Replaced getallheaders -function call with _SERVER

--- js/js.php
+++ js/js.php
@@ -87,9 +87,8 @@ else {
 $js .= $js_static;
 
 $hash = md5($js);
-$headers = getallheaders();
 // if Browser sent ID, we check if they match
-if(!empty($headers['If-None-Match']) AND ereg($hash, $headers['If-None-Match'])) {
+if( !empty( $_SERVER['HTTP_IF_NONE_MATCH'] ) AND ereg( $hash, $_SERVER['HTTP_IF_NONE_MATCH'] ) ) {
 	header('HTTP/1.1 304 Not Modified');
 }
 else {
