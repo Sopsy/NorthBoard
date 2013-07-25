ff804ea1e01e674dbd0538d6d10e976031b6ce83
Kehet <git@kehet.com>
2013-07-25 22:12:02 +0300

    removed accidentally committed comments

--- inc/functions_general.php
+++ inc/functions_general.php
@@ -43,8 +43,8 @@ function initialize() {
 		$warnings .= sprintf(T_("%s was not found from %s!"), "OptiPNG", $cfg['optipng_bin']) .' '. sprintf(T_('Please check the value of %s or change %s to false in config.php.'), '$cfg["optipng_bin"]', '$cfg["use_optipng"]') .' ('. sprintf(T_('Debian package name: %s'), 'optipng') .")\r\n";
 	if(!is_file($cfg['pngcrush_bin']) AND $cfg['use_pngcrush'])
 		$warnings .= sprintf(T_("%s was not found from %s!"), "PNGCrush", $cfg['pngcrush_bin']) .' '. sprintf(T_('Please check the value of %s or change %s to false in config.php.'), '$cfg["pngcrush_bin"]', '$cfg["use_pngcrush"]') .' ('. sprintf(T_('Debian package name: %s'), 'pngcrush') .")\r\n";
-	//if(is_file($cfg['srvdir'] ."/install/install.php"))
-	//	$warnings .= T_("You have not deleted the file install.php. This is a serious security issue!");
+	if(is_file($cfg['srvdir'] ."/install/install.php"))
+		$warnings .= T_("You have not deleted the file install.php. This is a serious security issue!");
 	
 	// If they weren't, kill the script.
 	if($warnings != '') {
